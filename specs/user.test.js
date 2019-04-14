c0df0f634ea77039342009aa2c1ac84070470d24
Johnbosco <boscoseries@gmail.com>
2019-04-14 11:42:53 +0100

    Add test case to validate method that will fetch a user by Id

--- specs/user.test.js
+++ specs/user.test.js
@@ -10,6 +10,16 @@ describe("User tests", function () {
     expect(received).toEqual(expect.objectContaining({ name: 'JOHN', email: 'john@gmail.com', password: 'johnpass' }))
   });
 
+  test("Expect getUserById(2) to get the user with id 2", function() {
+    var create_user = users.createUser('bashy', 'bashy@gmail.com', 'bahypass');
+    var received = users.getUserById(2);
+    expect(received).toHaveProperty('id', 2);
+  });
+
+  test("Expect getUserById(1000) to return false", function() {
+    var received = users.getUserById(1000);
+    expect(received).toBeFalsy();
+  });
 
 
 })
