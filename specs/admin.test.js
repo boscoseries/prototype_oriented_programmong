0665c67af48e2cdd4d712fcb2229580686a6db53
Johnbosco <boscoseries@gmail.com>
2019-04-14 12:18:02 +0100

    Add test cases that will validate admin function to detele user by Id

---
+++ specs/admin.test.js
@@ -0,0 +1,19 @@
+var Admin = require('../admin');
+var admin = new Admin();
+
+describe("Admin user tests", function () {
+
+  test("Expect deleteUserById(2) to delete the user with Id = 2", function () {
+    var new_user= admin.createUser('new Bash', 'newbash@gmail.com', 'newbashpass');
+    var received = admin.deleteUserById(1);
+    var userId_1 = admin.getUserById(1);
+    expect(received).toContain('user deleted');
+    expect(userId_1).toBeFalsy();
+  });
+
+  test("Expect deleteUserById(2000) to return 'user not found'", function () {
+    var delete_user = admin.deleteUserById(2000);
+    expect(delete_user).toContain('user not found');
+  });
+
+})
