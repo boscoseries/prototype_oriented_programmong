2eae28a2a2476e0081290dbc293f04203c96f30f
Johnbosco <boscoseries@gmail.com>
2019-04-14 12:19:26 +0100

    Add method for admin to delete one user

---
+++ admin.js
@@ -0,0 +1,41 @@
+var all_users = require('./database');
+var User = require('./users');
+
+
+
+function Admin() {
+  User.call(this);
+}
+
+// set Admin user to inherit the User properties and prototypes
+Admin.prototype = Object.create(User.prototype);
+
+// set the Admin constructor to point to the User constructor
+Admin.prototype.constructor = Admin;
+
+Admin.prototype.deleteUserById = function (id) {
+  var isAUser = admin.getUserById(id);
+  if (isAUser) {
+    var index = all_users.indexOf(isAUser);
+    all_users.splice(index, 1);
+    return 'user deleted';
+  }
+  else {
+    return 'user not found';
+  };
+};
+
+
+var admin = new Admin();
+
+
+
+//admin.createUser('Bash', 'bash2@gmail.com', 'bashpass');
+//admin.createUser('Paul', 'paul2@gmail.com', 'paulpass');
+
+
+//console.log(user.getAllUsers());
+
+
+
+module.exports = Admin;
