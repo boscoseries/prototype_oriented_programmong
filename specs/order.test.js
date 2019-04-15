40a652a13d85682b7e72e330d816bccaa54aef48
Johnbosco <boscoseries@gmail.com>
2019-04-15 13:47:00 +0100

    Add more test cases to the create orders method

--- specs/order.test.js
+++ specs/order.test.js
@@ -2,8 +2,23 @@ var order = require('../orders');
 var user = require('../users');
 
 describe("order tests", function() {
-  test("createOrder(1, 1, { book: 5, biro: 2}) should create an order with the products", function() {
+
+  test("createOrder(1, { book: 5, biro: 2}) should create an order with the products 'book and biro", function() {
     var received = order.createOrder(1, 1, { book: 5, biro: 2})
-    expect(received).toBeDefined()
+    expect(received).toBeDefined();
   })
+
+  test("createOrder(1, 1, { book: 5, biro: 2}) should create an order with the products 'book and biro", function() {
+    var received = order.createOrder(1, { book: 5, biro: 2})
+    expect(received.products).toHaveProperty('book', 5);
+  })
+
+  test("createOrder(10000, { book: 5, biro: 2}) should return 'user not found", function() {
+    var received = order.createOrder(10000, 1, { book: 5, biro: 2})
+    expect(received).toContain('user not found');
+  })
+
+
+
+
 })
